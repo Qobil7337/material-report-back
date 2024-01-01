be91bed86cba7eb21355cb7c6cd46b26b835ea37
Qobil7337 <qobil7337@mail.ru>
2024-01-01 20:09:32 +0500

    a

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as fs from "fs";
 
-const httpsOptions = {
-  key: fs.readFileSync('/etc/letsencrypt/live/167.71.83.248/privkey.pem'),
-  cert: fs.readFileSync('/etc/letsencrypt/live/167.71.83.248/fullchain.pem'),
-};
+
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { httpsOptions });
+  const app = await NestFactory.create(AppModule);
   app.enableCors();
   await app.listen(4000);
 }
